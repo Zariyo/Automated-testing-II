af41c9b850774ea1cef1f4b9815344bf73a427af
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2022-01-27 19:36:22 +0100

    Add files via upload

--- tests/test_Register_Mock.py
+++ tests/test_Register_Mock.py
@@ -13,7 +13,8 @@ class test_Register_Mock(unittest.TestCase):
                              Student(42, "Jan", "Kowalski", [{"subject": "geography", "notes": []}]).returnObject(),
                              Student(55, "Kamil", "Stoszek", [{"subject": "maths", "notes": [4, 6, 2, 3.5]},
                                                               {"subject": "geography",
-                                                               "notes": [1, 2, 3]}]).returnObject()])
+                                                               "notes": [1, 2, 3]}]).returnObject(),
+                             Student(10, "Kamil", "Michalski", [{"subject": "maths", "notes": []}]).returnObject()])
         self.database = Database()
         self.database.read_students = Mock(name="read_students")
         self.database.add_student = Mock(name="add_student")
@@ -31,6 +32,12 @@ class test_Register_Mock(unittest.TestCase):
         self.database.add_notice = Mock(name="add_notice")
         self.database.del_notice = Mock(name="del_notice")
         self.database.edit_notice = Mock(name="edit_notice")
+
+    def get_notes_fake_mock(self):
+        return [2,2,3,3.5]
+
+    def add_student_fake_mock(self):
+        return {"id": 205852, "name": "Paweł","surname": "Szendzielski", "subjects": [], "notices": []}
 
     def test_read_students_mock(self):
         self.assertIsInstance(self.database.read_students, Mock)
@@ -104,7 +111,16 @@ class test_Register_Mock(unittest.TestCase):
 
         self.assertEqual(self.database.add_student("student"), self.register.add_Student(521, "Jan", "Janosik"))
 
-    def test_del_student_mock_side_effect(self):
+    def test_edit_student_mock_create_autospec_typeError(self):
+        x = create_autospec(self.register)
+        self.assertRaises(TypeError, x.edit_Student, 15, "maths", "geography", "physics", "history")
+
+    def test_get_notes_mock_create_autospec_typeError(self):
+        x = create_autospec(self.register)
+        self.assertRaises(TypeError, x.get_notes, 15)
+
+
+    def test_del_student_mock_side_effect_exception(self):
         self.database.del_student.side_effect = ({"id": 42, "name": "Jan", "surname": "Kowalski",
                            "subjects": [
                                {"subject": "geography", "notes": [1, 3.40, 5.12, 4.13]}],
@@ -122,6 +138,45 @@ class test_Register_Mock(unittest.TestCase):
 
         assert_that(self.database.add_subject(15, "maths")).is_instance_of(object)
 
-    def test_del_subject_mock_create_autospec(self):
+    def test_del_subject_mock_create_autospec_typeError(self):
         x = create_autospec(self.register)
         self.assertRaises(TypeError, x.remove_subject, 15, "maths", "adam")
+
+    def test_add_subject_mock_create_autospec_typeError(self):
+        x = create_autospec(self.register)
+        self.assertRaises(TypeError, x.add_subject, 15, "maths", "adam")
+
+    def test_add_notes_mock_return_value(self):
+        self.database.add_notes.return_value = {"id": 10, "name": "Kamil", "surname": "Michalski",
+                           "subjects": [
+                               {"subject": "maths", "notes": [3,4,5]}],
+                           "notices": []}
+        self.assertEqual(self.database.add_notes(), self.register.add_notes(10, "maths", [3,4,5]))
+
+    def test_read_notes_magic_mock(self):
+        self.database.read_notes = MagicMock(return_value=[4,6,2,3.5])
+        self.database.read_notes(55, "maths")
+        self.database.read_notes.assert_called_with(55, "maths")
+
+    def test_read_notices_magic_mock(self):
+        self.database.read_notices = MagicMock(return_value=["Złe zachowanie."])
+        self.database.read_notices(55)
+        self.database.read_notices.assert_called_once()
+
+    def test_get_student_average_magic_mock(self):
+        self.register.get_student_average = MagicMock(return_value=3.2)
+        self.register.get_student_average(55)
+        self.register.get_student_average.assert_called_once_with(55)
+
+    def test_del_subject(self):
+        self.database.del_subject = MagicMock(return_value={"Student"})
+        self.database.del_subject.assert_not_called()
+
+    def test_add_student_fake_mock(self):
+        self.assertEqual(self.add_student_fake_mock(), self.register.add_Student(205852, "Paweł", "Szendzielski"))
+
+    def test_get_notes_fake_mock(self):
+        self.register.add_Student(205852, "Paweł", "Szendzielski")
+        self.register.add_subject(205852, "maths")
+        self.register.add_notes(205852, "maths", [2, 2, 3, 3.5])
+        self.assertEqual(self.get_notes_fake_mock(), self.register.get_notes(205852, "maths"))
